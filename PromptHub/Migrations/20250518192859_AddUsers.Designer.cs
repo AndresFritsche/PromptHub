befa48b30ff2b5c46638e11a23b92b8271edbade
andresf0090@gmail.com <andresf0090@gmail.com>
2025-05-18 21:54:57 -0400

    improved prompt UI

--- PromptHub/Migrations/20250518192859_AddUsers.Designer.cs
+++ PromptHub/Migrations/20250518192859_AddUsers.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PromptHub.Data;
 
@@ -10,9 +11,11 @@ using PromptHub.Data;
 namespace PromptHub.Migrations
 {
     [DbContext(typeof(PromptHubContext))]
-    partial class PromptHubContextModelSnapshot : ModelSnapshot
+    [Migration("20250518192859_AddUsers")]
+    partial class AddUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- client/src/Pages/PromptsPage.tsx
+++ client/src/Pages/PromptsPage.tsx
@@ -1,11 +1,14 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import FetchPrompts from "../components/Prompt";
+
 
 const PromptsPage = () => {
   return (
     <>
       <Navbar />
       <FetchPrompts />
+      <Footer />
     </>
   );
 };
