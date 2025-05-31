6a1997007646c62a10638cc8869f75a089c0c9df
DESKTOP-H28N80S\Productivity <andresf0090@gmail.com>
2025-05-31 15:03:28 -0400

    removed .net backend app

--- PromptHub/Dto/Category/CategoryCreateDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PromptHub.Dto.Category
-{
-    public class CategoryCreateDto
-    {
-        public string? Title { get; set; }
-    }
-}
--- PromptHub/Dto/PromptDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PromptHub.Dto
-{
-    public class PromptDto
-    {
-        public string? Title { get; set; }
-        public string? Content { get; set; }
-        public string? Category { get; set; }
-    }
-}
